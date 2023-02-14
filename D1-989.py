9d3d9801b7da36b0008e21a067491aed5cbeeb84
JadeLiu13 <49514060+JadeLiu13@users.noreply.github.com>
2023-02-14 23:57:26 +0800

    Update D1-989.py

--- D1-989.py
+++ D1-989.py
@@ -1,23 +1,24 @@
-###
+* https://leetcode.cn/problems/add-to-array-form-of-integer/ *
+# 案例https://leetcode.cn/problems/add-to-array-form-of-integer/solution/989-shu-zu-xing-shi-de-zheng-shu-jia-fa-myhvh/  
+'''
+思路：
 K取余作为相加的位数，K取整作为进一位的相加结果
-###
+'''
 class Solution:
     def addToArrayForm(self, A: list[int], K: int) -> list[int]:
         n = len(A)
         ans = []
 
         for i in range(n-1, -1, -1):
-            total = A[i] + K % 10
-
+            sum = A[i] + K % 10
             K //= 10
-            
-            if total >= 10:
-                #当前位相加结果大于 10 时，进位 1 加入下一位计算
+            # 当前位相加结果大于 10 时，进位 1 加入下一位计算
+            if sum >= 10:                
                 K += 1
-
+            # 进入数组
             ans.append(total % 10)
             
-        # 这里考虑数组 A 的长度小于整数 K 位数个数的情况
+        # 若数组 A 的长度小于整数 K 位数个数的情况
         while K > 0:
             ans.append(K % 10)
             K //= 10
@@ -25,3 +26,8 @@ class Solution:
         return ans[::-1]
 ans = Solution().addToArrayForm([1,2,0,0],44)
 print(ans)
+
+'''
+时间复杂度：O(n)
+空间复杂度：O(n)
+'''
